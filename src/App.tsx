53d992e4a9b4a38f6b3b272618580497e5a144e1
Shawnx2013 <49568498+Shawnx2013@users.noreply.github.com>
2023-06-07 18:14:18 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ClickCounter } from './ClickCounter';
 import './index.css';
 
 const App = () => {
-  const name = "Shawn here";
+  const name = "Shawn there";
   return (
     <>
       <h1>Hello world there</h1>
