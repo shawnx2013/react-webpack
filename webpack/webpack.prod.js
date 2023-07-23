abf6981e3c25901f56bccf5ee0fb394856e7dbe0
Shawnx2013 <49568498+Shawnx2013@users.noreply.github.com>
2023-07-22 22:50:15 -0400

    fixed BundleAnalyzerPlugin import

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'production',
